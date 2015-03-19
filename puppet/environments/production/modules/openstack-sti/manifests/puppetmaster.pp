a27075312ade487376f177e77fb44d0752d37b0c
root <root@ostest1.epfl.ch>
2015-03-19 11:54:49 +0100

    Make puppetmaster a smart class

--- puppet/environments/production/modules/openstack-sti/manifests/puppetmaster.pp
+++ puppet/environments/production/modules/openstack-sti/manifests/puppetmaster.pp
@@ -6,17 +6,19 @@
 # manages bare-metal provisioning (using Foreman) in addition to being
 # the puppetmaster.
 
-class openstack-sti::puppetmaster {
+class openstack-sti::puppetmaster(
+  $src_dir = "/opt/src/epfl.openstack-sti.foreman"
+  ) {
       package { 'puppet':
               ensure => 'present',
       }
       exec { 'latest_hammer':
-        command => "/opt/src/epfl.openstack-sti.foreman/puppet/scripts/install_latest_hammer",
-        unless => "/opt/src/epfl.openstack-sti.foreman/puppet/scripts/install_latest_hammer --check-only"
+        command => "${src_dir}/puppet/scripts/install_latest_hammer",
+        unless => "${src_dir}/puppet/scripts/install_latest_hammer --check-only"
       }
       exec { 'configure_discovery_templates':
-        command => "/opt/src/epfl.openstack-sti.foreman/puppet/scripts/configure_discovery_templates",
-        unless => "/opt/src/epfl.openstack-sti.foreman/puppet/scripts/configure_discovery_templates --check-only",
+        command => "${src_dir}/puppet/scripts/configure_discovery_templates",
+        unless => "${src_dir}/puppet/scripts/configure_discovery_templates --check-only",
         require => Exec["latest_hammer"],
       }
         
