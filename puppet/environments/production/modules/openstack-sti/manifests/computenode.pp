5280f9b9f879e37d99aebaa0699be2f6f5b429f8
root <root@ostest1.cloud.epfl.ch>
2015-03-30 19:17:08 +0200

    openstack-sti::computenode has mesos::slave

--- puppet/environments/production/modules/openstack-sti/manifests/computenode.pp
+++ puppet/environments/production/modules/openstack-sti/manifests/computenode.pp
@@ -4,4 +4,12 @@
 #
 # All nodes ought to be completely interchangeable.
 class openstack-sti::computenode() inherits openstack-sti {
+    class { "mesos":
+      repo => "mesosphere",
+      manage_python => true
+    }
+    class { "mesos::slave":
+      # Work around https://projects.puppetlabs.com/issues/11989
+      force_provider => "upstart"
+    }
 }
