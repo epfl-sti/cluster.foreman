53397a4ee92da576db8dd39dd3ecaed76ef60367
root <root@ostest1.cloud.epfl.ch>
2015-04-01 12:55:49 +0200

    Add $quorum_node_id and $quorum_node_id_max params
    Foreman is supposed to handle both of these (documented that requirement
    in
    https://github.com/epfl-sti/epfl.openstack-sti.foreman/wiki/Getting-started-with-Foreman)

--- puppet/environments/production/modules/openstack-sti/manifests/quorumnode.pp
+++ puppet/environments/production/modules/openstack-sti/manifests/quorumnode.pp
@@ -5,7 +5,28 @@
 # kept low and constant (say 3 or 5). In low count clusters, nodes may use
 # belong to both the openstack-sti::quorumnode and openstack-sti::computenode
 # classes.
-class openstack-sti::quorumnode() inherits openstack-sti {
+class openstack-sti::quorumnode(
+  $quorum_node_id = undef,      # Must be an int between 1 and $count
+  $quorum_node_id_max = undef,  # Must be an int
+  ) inherits openstack-sti {
+    if !(is_integer($quorum_node_id)) {
+      fail('$quorum_node_id must be an integer')
+    }
+    if !(is_integer($quorum_node_id_max)) {
+      fail('$quorum_node_id_max must be an integer')
+    }
+    if !($quorum_node_id >= 1) {
+      fail("$quorum_node_id must be positive (got ${quorum_node_id})")
+    }
+    if !($quorum_node_id <= $quorum_node_id_max) {
+      fail("$quorum_node_id (${$quorum_node_id_}) must be no larger than $quorum_id_max (got ${quorum_node_id_max})")
+    }
+    notice("This is node ${quorum_node_id} of ${quorum_node_id_max}")
+#    class { 'zookeeper':
+#      packages             => ['zookeeper', 'zookeeper-server'],
+#      service_name         => 'zookeeper-server',
+#      initialize_datastore => true
+#    }
     class { "mesos":
       repo => "mesosphere",
       manage_python => true
