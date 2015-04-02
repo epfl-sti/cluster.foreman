1e05ed5b0007aaf3cf2b038c6a5abf5fb4e939b7
root <root@ostest1.cloud.epfl.ch>
2015-04-02 12:41:40 +0200

    Gratuitously rename container

--- docker-foreman-installer/run.sh
+++ docker-foreman-installer/run.sh
@@ -3,11 +3,11 @@
 set -e -x
 
 # Build the docker
-docker build -t epflsti/foreman . 
+docker build -t epflsti/foreman-test-install . 
 
 # Run the Dockerfile
 SCRIPT_DIR="$(cd $(dirname "$0") && cd .. && pwd)"
 docker run \
     -v "$SCRIPT_DIR":/opt/src/epfl.openstack-sti.foreman \
     -h ostest0.epfl.ch \
-    -it epflsti/foreman /bin/bash
+    -it epflsti/foreman-test-install /bin/bash
