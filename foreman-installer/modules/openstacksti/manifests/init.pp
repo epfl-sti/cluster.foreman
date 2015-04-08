331afea962b80afac458b0caf311272e9fada944
Dominique Quatravaux <dom@MacBook-Air-de-Dominique.local>
2015-04-08 08:08:55 +0200

    New parameter src_path
    Duly set by configure.pl

--- foreman-installer/modules/openstacksti/manifests/init.pp
+++ foreman-installer/modules/openstacksti/manifests/init.pp
@@ -14,10 +14,16 @@
 #
 # $configure_answers::  Ignored - This is a placeholder for the configure.pl
 #                       script to persist the answers provided by the user
-#
+# $src_path::           The path where epfl-sti/
+#                       epfl.openstack-sti.foreman.git is checked out
 class openstacksti(
    $configure_answers = {},
+   $src_path = undef,
   ) {
+  if $src_path == undef {
+    fail '$src_path must be set'
+  }
+
   # For now, just a test to demonstrate that the Puppet code gets executed:
   file { "/etc/a-winner-is-you":
     ensure => "directory"
