ca8356ce17995d163b35d4505572d252e984b301
Dominique Quatravaux <dominique@quatravaux.org>
2015-04-11 11:04:36 +0200

    Reference the Wiki page instead of README.md

--- install-provisioning-server.sh
+++ install-provisioning-server.sh
@@ -20,7 +20,8 @@
 #  * repeatable: it should be okay to run it twice
 #  * readable (with comments in english)
 #  * minimalistic: complicated things should be done with Puppet instead
-#                  (see README.md for how to do what where)
+#                  (see https://github.com/epfl-sti/cluster.foreman/wiki/Hacking
+#                  for how to do what where)
 
 set -e -x
 
