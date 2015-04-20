711f46e4f964ea04233b578e604d7a5485e07eff
Dominique Quatravaux <dominique@quatravaux.org>
2015-04-20 18:54:47 +0200

    Harden foreman_provisioning against other race situations
    * Make sure to remove /var/lib/puppet/yaml/foreman/$(hostname
      -f)-push-facts.yaml so as to ensure that the push gets made

--- foreman-installer/modules/foreman_provisioning/manifests/init.pp
+++ foreman-installer/modules/foreman_provisioning/manifests/init.pp
@@ -10,31 +10,37 @@
 #
 # === Parameters:
 # 
-# $interface::          The name of the network interface connected to the
-#                       provisioning network
-# $domain_name::        The domain name to set on provisioned hosts
-# $network_address::    The IP network address of the provisioning network
-# $netmask::            The netmask of the network to provision into
-# $gateway::            The gateway to set on provisioned hosts
-# $dns::                The IP of the DNS server to set on provisioned hosts
-# $dhcp_range::         Range of addresses to allocate for provisioning
-# $foreman_topdir::     The directory where to run "${rails} runner" from
-# $state_dir::          The directory where to write state files
-# $rails::              The full path of the rails utility
-# $puppet_facts_dir::   The directory that contains Puppet-generated YAML fact
-#                       files for each host (in order to add one for this host)
+# $interface::            The name of the network interface connected to the
+#                         provisioning network
+# $domain_name::          The domain name to set on provisioned hosts
+# $network_address::      The IP network address of the provisioning network
+# $netmask::              The netmask of the network to provision into
+# $gateway::              The gateway to set on provisioned hosts
+# $dns::                  The IP of the DNS server to set on provisioned hosts
+# $dhcp_range::           Range of addresses to allocate for provisioning
+# $foreman_topdir::       The directory where to run "${rails} runner" from
+# $state_dir::            The directory where to write state files
+# $rails::                The full path of the rails utility
+# $puppet_facts_dir::     The directory that contains Puppet-generated YAML
+#                         fact files for each host (in order to add one for
+#                         this host)
+# $facts_push_stamp_dir:: The directory where /etc/puppet/node.rb keeps the
+#                         stamp files regarding the facts it pushes
+# $node_rb::              The path to the node.rb script
 class foreman_provisioning(
-  $interface           = undef,
-  $domain_name         = undef,
-  $network_address     = undef,
-  $netmask             = undef,
-  $gateway             = undef,
-  $dns                 = undef,
-  $dhcp_range          = undef,
-  $foreman_topdir      = $::foreman_provisioning::params::foreman_topdir,
-  $state_dir           = $::foreman_provisioning::params::state_dir,
-  $rails               = $::foreman_provisioning::params::rails,
-  $puppet_facts_dir    = $::foreman_provisioning::params::puppet_facts_dir,
+  $interface            = undef,
+  $domain_name          = undef,
+  $network_address      = undef,
+  $netmask              = undef,
+  $gateway              = undef,
+  $dns                  = undef,
+  $dhcp_range           = undef,
+  $foreman_topdir       = $::foreman_provisioning::params::foreman_topdir,
+  $state_dir            = $::foreman_provisioning::params::state_dir,
+  $rails                = $::foreman_provisioning::params::rails,
+  $puppet_facts_dir     = $::foreman_provisioning::params::puppet_facts_dir,
+  $facts_push_stamp_dir = $::foreman_provisioning::params::facts_push_stamp_dir,
+  $node_rb              = $::foreman_provisioning::params::node_rb
   ) inherits foreman_provisioning::params {
   validate_string($interface, $domain_name, $network_address, $netmask,
                   $gateway, $dns, $dhcp_range)
@@ -85,8 +91,9 @@ class foreman_provisioning(
   # Fact objects, including the network configuration, to meet prerequisites
   # #1 and #3 of foreman_setup's Web wizard.
   exec { "upload ${puppet_facts_file}" :
-    command => "/etc/puppet/node.rb --push-facts",  # See above re dependencies
-    unless => "/usr/bin/test -f '${breadcrumb_files[upload_facts]}'"
+    command => "/bin/rm ${facts_push_stamp_dir}/${::fqdn}-push-facts.yaml ; ${node_rb} --push-facts",
+    unless => "/usr/bin/test -f '${breadcrumb_files[upload_facts]}'",
+    require => File["/etc/puppet/node.rb"]
   } ->
   file { "${breadcrumb_files[upload_facts]}":
     ensure => "present",
--- foreman-installer/modules/foreman_provisioning/manifests/params.pp
+++ foreman-installer/modules/foreman_provisioning/manifests/params.pp
@@ -3,4 +3,6 @@ class foreman_provisioning::params {
   $foreman_topdir = "/usr/share/foreman"
   $state_dir = "/var/lib/foreman-installer/foreman_provisioning"
   $puppet_facts_dir = "/var/lib/puppet/yaml/facts"
+  $facts_push_stamp_dir = "/var/lib/puppet/yaml/foreman"
+  $node_rb = "/etc/puppet/node.rb"
 }
