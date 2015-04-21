19b7021ebc3c995743657522fb292709afac24d6
Dominique Quatravaux <dominique@quatravaux.org>
2015-04-21 08:15:53 +0200

    Update doc for init.pp

--- foreman-installer/modules/epflsti/manifests/init.pp
+++ foreman-installer/modules/epflsti/manifests/init.pp
@@ -2,18 +2,19 @@
 #
 # Customizations to foreman-installer that are specific to EPFL STI.
 #
-# This file is being symlinked as
-# /usr/share/foreman-installer/modules/epflsti/manifests/init.pp by
-# configure.pl, and then loaded by foreman-installer
-# as part of its job.
+# The epflsti directory gets symlinked as
+# /usr/share/foreman-installer/modules/epflsti by configure.pl, and
+# then foreman-installer loads the manifests/init.pp therein as part
+# of its job.
 #
 # === Parameters:
 #
 # $configure_answers::      Ignored - This is a placeholder for the
-#                           configure.pl script to persist the answers
-#                           provided by the user
-# $src_path::               The path where epfl-sti/cluster.foreman.git is
-#                           checked out
+#                           configure.pl script to persist the user-provided
+#                           answers that don't have a specific YAML
+#                           configuration entry of their own  
+# $src_path::               The path where epfl-sti/cluster.foreman is
+#                           checked out (required)
 class epflsti(
   $configure_answers = {},
   $src_path = undef,
@@ -21,9 +22,12 @@ class epflsti(
   validate_absolute_path($src_path)
 
   foreman::plugin {'column_view':
+    # Loaded from ../templates:
     config => template('epflsti/foreman_column_view_yaml.erb')
   }
 
+  # Import that class ("declare" it in Puppet lingo), with those
+  # parameters, from puppetconfig.pp in the same directory:
   class { "epflsti::puppetconfig":
     production_env_dir => "${src_path}/puppet/environments/production"
   }
