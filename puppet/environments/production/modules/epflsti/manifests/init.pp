d01def7eed21ae5f52f5bac9dd8eafeea899912b
ponsfrilus <ponsfrilus@gmail.com>
2015-05-20 13:57:35 +0200

    Update

--- puppet/environments/production/modules/epflsti/manifests/init.pp
+++ puppet/environments/production/modules/epflsti/manifests/init.pp
@@ -1,50 +1,59 @@
 # Class: epflsti
 #
-# Base class for epflsti::puppetmaster, epflsti::computenode
-# and epflsti::quorumnode.
+# Single entry point for all EPFLSTI cluster nodes.
 #
-# You probably want to use one of the subclasses from Foreman or puppet.conf.
+# Class parameters specify the functionality.
+#
+# === Parameters
+#
+# [*is_openstack_compute_node*]
+#   A Boolean
+#
 class epflsti(
-  $ensure         = 'present',
+  $is_openstack_compute_node = false,
 ) {
-  case $::osfamily {
-    'RedHat': {
-      if $::operatingsystemmajrelease = '6' {
-        package { 'puppetlabs-release-6':
-          ensure => 'latest',
-          provider => 'rpm',
-          source => 'https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm'
+    case $::osfamily {
+      'RedHat': {
+        if $::operatingsystemmajrelease = '6' {
+          package { 'puppetlabs-release-6':
+            ensure => 'latest',
+            provider => 'rpm',
+            source => 'https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm'
+          }
+          package { 'puppet':
+            ensure => 'latest',
+            require => Package['puppetlabs-release-6']
+          }
+          package { 'facter':
+            ensure => 'latest',
+            require => Package['puppetlabs-release-6']
+          }
+        } elsif $::operatingsystemmajrelease = '7' {
+          package { 'puppetlabs-release-7':
+            ensure => 'latest',
+            provider => 'rpm',
+            source => 'https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm'
+          }
+          package { 'puppet':
+            ensure => 'latest',
+            require => Package['puppetlabs-release-7']
+          }
+          package { 'facter':
+            ensure => 'latest',
+            require => Package['puppetlabs-release-7']
+          }
+        } else {
+          warning( 'Invalid operatingsystemmajrelease in /etc/puppet/environments/production/modules/epflsti/manifests/init.pp' )
         }
-        package { 'puppet':
-          ensure => 'latest',
-          require => Package['puppetlabs-release-6']
-        }
-        package { 'facter':
-          ensure => 'latest',
-          require => Package['puppetlabs-release-6']
-        }
-      } elsif $::operatingsystemmajrelease = '7' {
-        package { 'puppetlabs-release-7':
-          ensure => 'latest',
-          provider => 'rpm',
-          source => 'https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm'
-        }
-        package { 'puppet':
-          ensure => 'latest',
-          require => Package['puppetlabs-release-7']
-        }
-        package { 'facter':
-          ensure => 'latest',
-          require => Package['puppetlabs-release-7']
-        }
-      } else {
-        warning( 'Invalid operatingsystemmajrelease in /etc/puppet/environments/production/modules/epflsti/manifests/init.pp' )
+      }
+      default: {
+          fail('Unsupported OS in /etc/puppet/environments/production/modules/epflsti/manifests/init.pp')
       }
     }
-    default: {
-        fail('Unsupported OS in /etc/puppet/environments/production/modules/epflsti/manifests/init.pp')
+    class { "ntp": }
+    class { "ipmi": }
+
+    if ($is_openstack_compute_node) {
+      class { "epflsti::private::openstack": }
     }
   }
-  class { "ntp": }
-  class { "ipmi": }
-}
