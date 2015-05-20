5e309ac4c0557f6e27e977056edf441ba00af975
Nicolas Borboën <ponsfrilus@gmail.com>
2015-05-20 14:42:51 +0200

    Buggy if, switch case instead

--- puppet/environments/production/modules/epflsti/manifests/init.pp
+++ puppet/environments/production/modules/epflsti/manifests/init.pp
@@ -14,36 +14,42 @@ class epflsti(
 ) {
     case $::osfamily {
       'RedHat': {
-        if $::operatingsystemmajrelease = '6' {
-          package { 'puppetlabs-release-6':
-            ensure => 'latest',
-            provider => 'rpm',
-            source => 'https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm'
+	# Testing CentOS 6
+        #if $::operatingsystemmajrelease = '6' {
+        case $::operatingsystemmajrelease {
+          '6': {
+	    package { 'puppetlabs-release-6':
+              ensure => 'latest',
+              provider => 'rpm',
+              source => 'https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm'
+            }
+            package { 'puppet':
+              ensure => 'latest',
+              require => Package['puppetlabs-release-6']
+            }
+            package { 'facter':
+              ensure => 'latest',
+              require => Package['puppetlabs-release-6']
+            }
           }
-          package { 'puppet':
-            ensure => 'latest',
-            require => Package['puppetlabs-release-6']
+          '7': {
+            package { 'puppetlabs-release-7':
+              ensure => 'latest',
+              provider => 'rpm',
+              source => 'https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm'
+            }
+            package { 'puppet':
+              ensure => 'latest',
+              require => Package['puppetlabs-release-7']
+            }
+            package { 'facter':
+              ensure => 'latest',
+              require => Package['puppetlabs-release-7']
+            }
           }
-          package { 'facter':
-            ensure => 'latest',
-            require => Package['puppetlabs-release-6']
+          default: {
+            fail("${::osfamily}-${::operatingsystemmajrelease} is not supported")
           }
-        } elsif $::operatingsystemmajrelease = '7' {
-          package { 'puppetlabs-release-7':
-            ensure => 'latest',
-            provider => 'rpm',
-            source => 'https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm'
-          }
-          package { 'puppet':
-            ensure => 'latest',
-            require => Package['puppetlabs-release-7']
-          }
-          package { 'facter':
-            ensure => 'latest',
-            require => Package['puppetlabs-release-7']
-          }
-        } else {
-          fail( 'Invalid operatingsystemmajrelease in /etc/puppet/environments/production/modules/epflsti/manifests/init.pp' )
         }
       }
       default: {
