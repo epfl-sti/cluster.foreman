8f643c8ad595236b4c7a72fa19bdeb187f4ee361
root <root@ostest1.cloud.epfl.ch>
2015-06-01 21:58:02 +0200

    Move OpenStack concerns into epflsti::private::openstack
    * Split main class parameter $is_compute_node into $is_openstack_worker
      and $is_mesos_worker
    * Install packstack on all $is_openstack_worker and $is_quorum_node
      nodes
    * Do The Right Thing between RedHat-ish versions 6.x and 7.x
    * Add packstack-answers.txt.erb to Git (was previously forgotten)

--- puppet/environments/production/modules/epflsti/manifests/init.pp
+++ puppet/environments/production/modules/epflsti/manifests/init.pp
@@ -7,8 +7,8 @@
 # === Parameters
 #
 # $is_puppetmaster::      True iff this node acts as the puppet master
-# $is_compute_node::      True iff computations (VMs and/or Dockers) can run on this
-#                         node
+# $is_openstack_worker::  True iff OpenStack VMs can run on this node
+# $is_mesos_worker::      True iff Mesos workloads can run on this node
 # $is_quorum_node::       True iff this host is dedicated to "rigid" services such
 #                         as ZooKeeper that require a fixed set of IP addresses
 #                         (as opposed to "floating" jobs running under some kind
@@ -17,7 +17,8 @@
 # $dns_domain::           The DNS domain that all nodes in the cluster live in
 class epflsti(
   $is_puppetmaster         = false,
-  $is_compute_node         = false,
+  $is_openstack_worker     = false,
+  $is_mesos_worker         = false,
   $is_quorum_node          = false,
   $quorum_nodes            = $::epflsti::private::params::quorum_nodes,
   $dns_domain              = $::epflsti::private::params::dns_domain
@@ -32,8 +33,12 @@ class epflsti(
       sudoer_group => "openstack-sti",
       allowed_users_and_groups => "(openstack-sti)"
     }
-
-    if ($is_openstack_compute_node) {
-      class { "epflsti::private::openstack": }
+    
+    if ($is_openstack_worker or $is_quorum_node) {
+      class { "epflsti::private::openstack":
+        is_compute_node => $is_openstack_worker,
+        is_quorum_node => $is_quorum_node,
+        quorum_nodes => $quorum_nodes
+      }
     }
 }
--- puppet/environments/production/modules/epflsti/manifests/private/openstack.pp
+++ puppet/environments/production/modules/epflsti/manifests/private/openstack.pp
@@ -2,14 +2,39 @@
 #
 # Install OpenStack from https://rdoproject.org/'s PackStack
 #
-class epflsti::private::openstack() {
+# === Parameters
+#
+# $is_compute_node::      True iff VMs can run on this node
+# $is_quorum_node::       True iff this host is dedicated to "rigid" services such
+#                         as RabbitMQ that require a fixed set of IP addresses
+# $quorum_nodes::         List of FQDNs of hosts that have $is_compute_node set
+class epflsti::private::openstack(
+  $is_compute_node       = false,
+  $is_quorum_node        = false,
+  $quorum_nodes          = []
+  ) {
   # https://serverfault.com/questions/111766/adding-a-yum-repo-to-puppet-before-doing-anything-else
   Yumrepo <| |> -> Package <| provider != 'rpm' |>
+
+  case $::operatingsystem {
+    'RedHat', 'CentOS': {
+      if ($::operatingsystemrelease =~ /^6/) {
+        $distro_specific_repo_subdir = "epel-6"
+      } elsif ($::operatingsystemrelease =~ /^7/) {
+        $distro_specific_repo_subdir = "epel-7"
+      } else {
+        fail "FAIL: operating system release not supported (${::operatingsystem} release ${::operatingsystemrelease})"
+      }
+    }
+    default: {
+      fail "FAIL: operating system not supported (${::operatingsystem})"
+    }
+  }
 
   yumrepo { 'openstack-havana':
     ensure => present,
     descr => "OpenStack Icehouse Repository",
-    baseurl => "https://repos.fedorapeople.org/repos/openstack/openstack-icehouse/epel-6/",
+    baseurl => "https://repos.fedorapeople.org/repos/openstack/openstack-icehouse/${distro_specific_repo_subdir}/",
     gpgcheck => 1,
     gpgkey => "https://raw.githubusercontent.com/puppetlabs/puppetlabs-openstack/master/files/RPM-GPG-KEY-RDO-Icehouse",
   }
