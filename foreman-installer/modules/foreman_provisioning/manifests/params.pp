d22135c6ba01fc24525f7fd83618fef7e88a5d32
root <root@ostest1.cloud.epfl.ch>
2015-07-10 21:28:25 +0200

    Remove old stuff

--- foreman-installer/modules/foreman_provisioning/manifests/params.pp
+++
@@ -1,8 +0,0 @@
-class foreman_provisioning::params {
-  $rails = "/usr/bin/ruby193-rails"
-  $foreman_topdir = "/usr/share/foreman"
-  $state_dir = "/var/lib/foreman-installer/foreman_provisioning"
-  $puppet_facts_dir = "/var/lib/puppet/yaml/facts"
-  $facts_push_stamp_dir = "/var/lib/puppet/yaml/foreman"
-  $node_rb = "/etc/puppet/node.rb"
-}
