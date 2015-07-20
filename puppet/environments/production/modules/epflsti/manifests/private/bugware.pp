88072d64f8ac11672c88be680fd15792cf7a972e
root <root@ostest1.cloud.epfl.ch>
2015-07-20 17:13:56 +0200

    No more Puppet in my foreman

--- puppet/environments/production/modules/epflsti/manifests/private/bugware.pp
+++
@@ -1,4 +0,0 @@
-class epflsti::private::bugware() {
-  # https://serverfault.com/questions/111766/adding-a-yum-repo-to-puppet-before-doing-anything-else
-  Yumrepo <| |> -> Package <| provider != 'rpm' |>
-}
--- puppet/environments/production/modules/epflsti/manifests/private/ipmi.pp
+++
@@ -1,6 +0,0 @@
-# Class: epflsti::private::ipmi
-#
-# Install and configure ipmi and its integration with Foreman
-class epflsti::private::ipmi() {
-  class { "::ipmi": }
-}
