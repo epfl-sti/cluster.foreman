33bb5af189cbebedbfdc83baf4fe3482abc71048
root <root@ostest1.cloud.epfl.ch>
2015-12-01 17:52:44 +0100

    Revert back to having our own foreman-installer module
    The plan is to e.g. run puppetdb-ssl-setup prior to starting up puppetdb

---
+++ docker/foreman-base/epflsti.pp
@@ -0,0 +1,13 @@
+# = Class: epflsti
+#
+# EPFLSTI-specific enhancements to foreman-installer
+#
+# === Parameters:
+#
+# $interactive_answers::   Ignored - This is just a placeholder for
+#                 the configure.pl script to persist the user-provided
+#                 answers
+class epflsti(
+  $interactive_answers = {}
+) {
+}
