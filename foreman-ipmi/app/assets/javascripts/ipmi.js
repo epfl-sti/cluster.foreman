f31de2c8c598608960185cfc0b527674f39d9dbb
root <root@ostest1.cloud.epfl.ch>
2016-06-15 21:20:04 +0200

    Bits and pieces of the /ipmi/index mini-app

---
+++ foreman-ipmi/app/assets/javascripts/ipmi.js
@@ -0,0 +1,8 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+
+alert("That work ! If you see this, please ask Nicolas ^^");
+
+// ⚠DOCKER! root@puppetmaster:/usr/share/foreman$ vim app/assets/javascripts/ipmi.js 
+
